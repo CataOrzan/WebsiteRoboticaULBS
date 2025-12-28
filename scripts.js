95dfd0bca9262c7e0fa658d83debc5cfb4ef1a96
CataOrzan <113476674+CataOrzan@users.noreply.github.com>
2025-12-28 17:33:04 +0200

    Major2026Update

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 function call() {
-  const inputs = [0, 1, 2, 3, 4].map(
+  const inputs = [0, 1, 2, 3, 4, 5].map(
     (i) => document.getElementById(`nr${i}`).value
   );
   const numbers = inputs.map((input) => Number(input));
   const sum = numbers.reduce((acc, n) => acc + n, 0);
   const cred = numbers.reduce(
-    (acc, n, i) => acc + n * [6, 4, 4, 6, 10][i],
+    (acc, n, i) => acc + n * [4, 2, 4, 5, 5, 10][i],
     0
   );
   const pond = (cred / 300) * 10;
-  const avg = sum / 5;
+  const avg = sum / 6;
 
   document.getElementById("art").innerHTML = parseFloat(avg).toFixed(2);
   document.getElementById("pnd").innerHTML = parseFloat(pond).toFixed(2);
